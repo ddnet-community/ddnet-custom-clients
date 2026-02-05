1ab33e9a8105f1c926beec4f82f945834451da6b
+KZ <60852359+M0REKZ@users.noreply.github.com>
2026-02-05 10:56:35 -0300

    Update custom_clients_ids.h

--- custom_clients_ids.h
+++ custom_clients_ids.h
@@ -5,7 +5,7 @@
 
 enum
 {
-	// Custom client IDs
+	// Custom client IDs (Country flag method)
 	CUSTOM_CLIENT_ID_KAIZO_NETWORK = 16777216,
 	CUSTOM_CLIENT_ID_PDUCKCLIENT = 16777217,
 	CUSTOM_CLIENT_ID_GAMER_07 = 16777218, //0.7 Client Reserved
@@ -17,7 +17,14 @@ enum
 	MINIMUM_CUSTOM_CLIENT_ID = CUSTOM_CLIENT_ID_KAIZO_NETWORK,
 	MAXIMUM_CUSTOM_CLIENT_ID = CUSTOM_CLIENT_ID_CHILLERBOTUX,
 
-	// Other info to be shared among clients
-	CUSTOM_CLIENT_ISDUMMY = 33554433,
-	CUSTOM_CLIENT_ISBOT = 33554434, // intended for clients like discord bridge bots
+	// Custom client IDs (Skin Color Method)
+	// Range is small (510 possible IDs)
+
+	// Body
+	CCID_COLOR_BODY_KAIZO_CLIENT = 1,
+	CCID_COLOR_BODY_PDUCKCLIENT = 2,
+	CCID_COLOR_BODY_CHILLERBOTUX = 3,
+
+	// Feet
+	// (Empty...)
 };
